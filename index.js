439610c2e332a8646f2924ed42a1b150b9900cf1
AbelBatalla <110605498+AbelBatalla@users.noreply.github.com>
2023-05-10 22:04:43 +0200

    ambFotoWORKING

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const functions = require("firebase-functions");
 const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
 const cors = require('cors')({ origin: true });
+const axios = require('axios');
+const { ImageType } = require('pdf-lib');
+const https = require('https');
+
 
 exports.generatePDF = functions.https.onRequest(async (req, res) => {
 
@@ -66,8 +70,23 @@ exports.generatePDF = functions.https.onRequest(async (req, res) => {
                 try {
                     const jsonObject = JSON.parse(data);
 
+
+                        const jpgUrl = 'http://agenda.cultura.gencat.cat/content/dam/agenda/articles/2023/04/14/006/000men(3).jpg';
+                        const agent = new https.Agent({ rejectUnauthorized: false });
+                        const response = await axios.get(jpgUrl, { responseType: 'arraybuffer', httpsAgent: agent });
+                        const jpgImageBytes = response.data;
+                    console.log("IMAGE:");
+                    console.log(jpgImageBytes);
+
+
+
+
                     // Create a new PDFDocument
-                    const pdfDoc = await PDFDocument.create();
+                        const pdfDoc = await PDFDocument.create();
+
+
+                        const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
+                        const jpgDims = jpgImage.scale(0.5);
 
                     // Set the font for the document
                     const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
@@ -80,7 +99,7 @@ exports.generatePDF = functions.https.onRequest(async (req, res) => {
                     if (jsonObject.hasOwnProperty('esdeveniment')) {
                         console.log("escrivint esdeveniment");
                         console.log(jsonObject.esdeveniment);
-                        page.drawText(jsonObject.esdeveniment+"\n", {
+                        page.drawText(jsonObject.esdeveniment, {
                             x: 50,
                             y: cursor,
                             size: 20,
@@ -129,6 +148,51 @@ exports.generatePDF = functions.https.onRequest(async (req, res) => {
                         cursor = cursor - 20;
                     }
 
+
+                        page.drawImage(jpgImage, {
+                            x: page.getWidth() / 2 - jpgDims.width / 2,
+                            y: page.getHeight() / 2 - jpgDims.height / 2 + 250,
+                            width: jpgDims.width,
+                            height: jpgDims.height,
+                        });
+
+
+/*
+                    //IMATGES:
+                    const qrCodeImage = await axios.get(
+                        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZoAAAGaAQAAAAAefbjOAAADDUlEQVR4nO2cS27bShBFT6UJeEgBbwFeCrUzI0vKDsilaAEGmkMDFO4bdHWTjkeJDEqhigODHx2oGyrU51bRJv74mH78OQMBBRRQQAEFFNAxIfOjw87lssPsBHaeO2CuHzjfZXkB7Q8NkqQMTCfQCGjkahrL8yRJ0mdov+UFtD80NwfA1c+GnGT2+mGA+427LS+gu0FSTmLIHkk0zh5Jvv2bAvo3oJI4TK8fBv2CnUlfFYt/bE8B/Q3US2v2wHDpAJLKJYCk5X7LC2hvaDIzLy6KU0hiuHTYW04CrqXUuNfyAtoPKtniGhgES40am3ufI8eD7ymgWyBKVTmsIaFfSjG6eUqvkm2WKnV88D0FdAvEqjNo7CU3hn7xxMHtwM+KKhEWcWSo+ohcswf6hWIRYyszytNqL2ERh4aaj/DfXGP5s6Cxr55hxIWK8BHHh2ogaE5hyOBRo2pVGklyAwmLODrUcsdlaxvKqXoLoDiP8BHPAVU7IHmnq1UdUqb5jeQpRFjE00C9y9b6ecKbXGeS7C1fzZ/O0Q1/Imi4vIjJOmDta7Rjsg6m11CxnwcyOwH0kr1J0rgxi+TJREk577K8gHZXsaF/N4aLocnSwqBrJ+YOwYeVy2n1Gg++p4BugZqKrSpOfc4xi1KZkzzHjMzy6FCrOVsjI9MiRNKqYrtMFRZxdGjT6fK+Rv5Nkqodjo1k8eB7CugWqM1OJhl0i2DpoM/A/CImA6YzMPzqs'
+                    );
+
+                    const qrCodeImageEmbed = await pdfDoc.embedPng(qrCodeImage.data);
+
+                    page.drawImage(qrCodeImageEmbed, {
+                        x: 250, // X-coordinate position of the image on the page
+                        y: 100, // Y-coordinate position of the image on the page
+                        width: 200, // Width of the image
+                        height: 200, // Height of the image
+                    });
+
+
+                    console.log("GETTING IMAGES");
+
+                    const imageBytes = await axios.get(
+                        'http://agenda.cultura.gencat.cat/content/dam/agenda/articles/2023/04/14/006/000men(3).jpg',
+                        { responseType: 'arraybuffer' }
+                    );
+
+                    console.log("EMBEDING IMAGES");
+
+                    const imageEmbed = await pdfDoc.embedJpg(imageBytes.data);
+
+                    console.log("WRITING IMAGES");
+
+                    page.drawImage(imageEmbed, {
+                        x: 100, // X-coordinate position of the image on the page
+                        y: 100, // Y-coordinate position of the image on the page
+                        width: 200, // Width of the image
+                        height: 200, // Height of the image
+                    });
+*/
                     // Get the PDF file as a buffer
                     const pdfBytes = await pdfDoc.save();
 
